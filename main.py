6fe53d0538fe15606853196e14ec44240adb1874
Samir Boumendjel <73393090+Miroo1010@users.noreply.github.com>
2022-04-26 10:56:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+life_expenctancy= 90
+life_left= life_expenctancy-int(age)
+print(f"You have {life_left*365} days, {life_left*52} weeks, and {life_left*12} months left.")
+
+
+
+
+
+
+
+
